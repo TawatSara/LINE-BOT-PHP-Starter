a68602a688c1b070cc62a5be367121f8444b4a19
TawatSara <Tawatsara@gmail.com>
2017-06-14 08:04:32 +0700

    Create writelogger.php

--- writelogger.php
+++ writelogger.php
@@ -1,5 +1,6 @@
 <?
 $a=fopen("logger.txt","a+");
+$b=fopen("current.txt","a+");
 //$d="####".date("d M Y H:i:s")."####\r\n";
 //$d="####".date("Y-m-d H:i:s")."####\r\n";
 $d="<{date: ".date("Y-m-d H:i:s")."}>"."\r\n";
