8a5bffb601e28d00aa28a42932bcd1fbcc8d8ff6
TawatSara <Tawatsara@gmail.com>
2017-06-14 08:44:12 +0700

    Create writelogger.php

--- writelogger.php
+++ writelogger.php
@@ -1,6 +1,7 @@
 <?
 //$a=fopen("logger.txt","a+");//อ่านค่าทั้งหมดมาด้วย
 $a=fopen("logger.txt","w");//เขียนทับไปเลย
+/*
 $strFileName = "current.txt";
 $objFopen = fopen($strFileName, 'r');
 if ($objFopen) {
@@ -8,16 +9,18 @@ if ($objFopen) {
     $file = fgets($objFopen, 4096);
     fputs($a,$file);
 }
+*/
+
 //$d="####".date("d M Y H:i:s")."####\r\n";
 //$d="####".date("Y-m-d H:i:s")."####\r\n";
 $d="<{date: ".date("Y-m-d H:i:s")."}>"."\r\n";
 fputs($a,$d);
-//fputs($a,"<{new: ");
-//fputs($a,$b);
-//fputs($a,"}>");
-//fputs($a,"\r\n");
+fputs($a,"<{new: ");
+fputs($a,$b);
+fputs($a,"}>");
+fputs($a,"\r\n");
 
-fclose($objFopen);
+//fclose($objFopen);
 fclose($a);
 
 echo ("OK");
