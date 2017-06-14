7c29566e603a5c3610fafacfadf69820c8fc08d4
TawatSara <Tawatsara@gmail.com>
2017-06-14 12:01:50 +0700

    Create writelogger.php

--- writelogger.php
+++ writelogger.php
@@ -11,31 +11,19 @@ if ($handle) {
     }
     fclose($handle);
 }
-echo $text1;
+//echo $text1;
+
+$fa = @fopen("current.txt", "a+");
+if ($fa) {
+    fputs($fa,$text1);
+    fclose($fa);
+}
 
 /*
-$text1='A';
-$fp = fopen('current.txt', 'r');
-if (!$fp) {
-    //echo 'Could not open file current.txt';
-}
-while (false !== ($char = fgetc($fp))) {
-    //$fc = iconv('windows-1250', 'utf-8', $char); 
-    $fc = iconv('tis-620', 'utf-8', $char);
-    echo "$fc";
-    //echo "$char\n";
-    //$text1=$char;
-    //ตัวอย่างต้องการแปลง utf-8 เป็น tis-620
-    //echo iconv( 'TIS-620', 'UTF-8', "สวัสดีครับ Mindphp.com");
-}
-fclose($fp);
-//echo ($text1);
-*/
-
-//$a=fopen("logger.txt","a+");//อ่านค่าทั้งหมดมาด้วย
+$a=fopen("logger.txt","a+");//อ่านค่าทั้งหมดมาด้วย
 //$a=fopen("logger.txt","w");//เขียนทับไปเลย
-//fputs($a,$text1);
-/*
+fputs($a,$text1);
+
 //$d="####".date("d M Y H:i:s")."####\r\n";
 //$d="####".date("Y-m-d H:i:s")."####\r\n";
 $d="<{date: ".date("Y-m-d H:i:s")."}>"."\r\n";
