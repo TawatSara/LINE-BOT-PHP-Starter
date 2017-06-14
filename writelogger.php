026317c9e02e7e88ff1b1424de4757aebab1ca0c
TawatSara <Tawatsara@gmail.com>
2017-06-14 11:52:54 +0700

    Create writelogger.php

--- writelogger.php
+++ writelogger.php
@@ -1,14 +1,17 @@
 <?php
+$text1='ABC';
 $handle = @fopen("current.txt", "r");
 if ($handle) {
     while (($buffer = fgets($handle, 4096)) !== false) {
-        echo $buffer;
+        //echo $buffer;
+        $text1 = $buffer;
     }
     if (!feof($handle)) {
-        echo "Error: unexpected fgets() fail\n";
+        //echo "Error: unexpected fgets() fail\n";
     }
     fclose($handle);
 }
+echo $text1;
 
 /*
 $text1='A';
