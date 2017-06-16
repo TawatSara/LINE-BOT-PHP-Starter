c9a5b636cbb965bd6e447c51794dd881f613d697
TawatSara <Tawatsara@gmail.com>
2017-06-16 08:50:33 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,66 +1,4 @@
 <?php
-/**
- * Eric Draken
- * Date: 2016-09-02
- * Time: 4:44 PM
- * Desc: Callback for responding to Line messages
- *       Send 'whoami' to this endpoint to get a reply with your mid.
- */
- 
-// I put constants like 'LINE_CHANNEL_ID' here 
-//require_once __DIR__ . '/../includes/config.php';
-require_once __DIR__ . "/../includes/line-bot-sdk/vendor/autoload.php";
- 
-use LINE\LINEBot;
-use LINE\LINEBot\HTTPClient\GuzzleHTTPClient;
- 
-// Set these values
-$config = [
-    'channelId' => LINE_CHANNEL_ID,
-    'channelSecret' => LINE_CHANNEL_SECRET,
-    'channelMid' => LINE_CHANNEL_MID,
-];
-$sdk = new LINEBot($config, new GuzzleHTTPClient($config));
- 
-$postdata = @file_get_contents("php://input");
-$messages = $sdk->createReceivesFromJSON($postdata);
- 
-// Verify the signature
-// REF: http://line.github.io/line-bot-api-doc/en/api/callback/post.html#signature-verification
-// REF: http://stackoverflow.com/a/541450
-$sigheader = 'X-LINE-ChannelSignature';
-$signature = @$_SERVER[ 'HTTP_'.strtoupper(str_replace('-','_',$sigheader)) ];
-if($signature && $sdk->validateSignature($postdata, $signature)) {
-    // Next, extract the messages
-    if(is_array($messages)) {
-        foreach ($messages as $message) {
-            if ($message instanceof LINEBot\Receive\Message\Text) {
-                $text = $message->getText();
-                if (strtolower(trim($text)) === "whoami") {
-                    $fromMid = $message->getFromMid();
-                    $user = $sdk->getUserProfile($fromMid);
-                    $displayName = $user['contacts'][0]['displayName'];
- 
-                    $reply = "You are $displayName, and your mid is:\n\n$fromMid";
- 
-                    // Send the mid back to the sender and check if the message was delivered
-                    $result = $sdk->sendText([$fromMid], $reply);
-                    if(!$result instanceof LINE\LINEBot\Response\SucceededResponse) {
-                        error_log('LINE error: ' . json_encode($result));
-                    }
-                } else {
-                    // Process normally, or do nothing
-                }
-            } else {
-                // Process other types of LINE messages like image, video, sticker, etc.
-            }
-        }
-    } // Else, error
-} else {
-    error_log('LINE signatures didn\'t match: ' . $signature);
-}
-
-/*
 //อ่านค่าจาก Text File
 $text1='ABC';
 $handle = @fopen("current.txt", "r");
@@ -94,9 +32,9 @@ if (!is_null($events['events'])) {
 			$text = $event['message']['text'];
 			// Get replyToken
 			$replyToken = $event['replyToken'];
-			$userId = $event['userId'];
-			$groupId = $event['groupId'];
-			$roomID = $event['roomID'];
+			$userId = $event['mid'];
+			$groupId = $event['access_token'];
+			$roomID = $event['client_id'];
 			$text .= $stringtemp . "\r\n" . $replyToken . "\r\n =>" . strlen($replyToken);
 			$text .= "\r\n UserId:" . $userId ;
 			$text .= "\r\n GroupId:" . $groupId ;
@@ -131,4 +69,3 @@ if (!is_null($events['events'])) {
 	}
 }
 echo "OK";
-*/
